25bf8c4918ffaa1a9be40c23e6d9558a82a5b7f1
Karol Vogelgezang <karol.vogelgezang@domeny.pl>
2019-06-19 16:25:51 +0200

    work

--- JavaScript.js
+++ JavaScript.js
@@ -7,45 +7,49 @@ ale sam kod jest raczej porpawny. Zrób z tego wiele plików np "Clousures", "Fu
 	// Miejsce w pamięci (adres)
 	// Wartość
 
-	// Dwa procesy
-		// Deklaracja
-		// Inicjalizacja (przypisanie wartości)
+		// Dwa procesy
+			// Deklaracja
+			// Inicjalizacja (przypisanie wartości)
 
-// var, let i const
+	// var, let i const
 
-	var a = "a - zakres globalny";
-	let b = "b - zakres globalny"
-	const c = "c - zakres globalny"
+		var a = "a - zakres globalny";
+		let b = "b - zakres globalny"
+		const c = "c - zakres globalny"
 
-	{
-		var a = "a - zakres lokalny";
-		let b = "b - zakres lokalny";
-		const c = "c - zakres lokalny"
-		console.log(a); // "a - zakres lokalny";
-		console.log(b); // "b - zakres lokalny";
-		console.log(c); // "c - zakres lokalny";
-	}
-
-
-console.log(a); // "a - zakres lokalny";
-console.log(b); // "b - zakres globalny";
-console.log(c); // "c - zakres globalny";
-
-// let, const - zasięg zmiennych w zakresie blokowym (w tym zakres funkcji). var - zakres funkcji (bez zakresu blokowego)
-
-// Ponowna deklaracja
-	// let,const - błąd
-		// przykład
-			// let a = 5;
-			// let a = 10; // błąd
-			// const a = 5;
-			// const a = 10; // błąd
-	// var - nadpisuje
-		// przykład
-			// var a = 5;
-			// var a = 10; // nie ma błędu
+		{
+			var a = "a - zakres lokalny";
+			let b = "b - zakres lokalny";
+			const c = "c - zakres lokalny"
+			console.log(a); // "a - zakres lokalny";
+			console.log(b); // "b - zakres lokalny";
+			console.log(c); // "c - zakres lokalny";
+		}
 
 
+	console.log(a); // "a - zakres lokalny";
+	console.log(b); // "b - zakres globalny";
+	console.log(c); // "c - zakres globalny";
+
+	// let, const - zasięg zmiennych w zakresie blokowym (w tym zakres funkcji). var - zakres funkcji (bez zakresu blokowego)
+
+	// Ponowna deklaracja
+		// let,const - błąd
+			// przykład
+				// let a = 5;
+				// let a = 10; // błąd
+				// const a = 5;
+				// const a = 10; // błąd
+		// var - nadpisuje
+			// przykład
+				// var a = 5;
+				// var a = 10; // nie ma błędu
+
+	// HOISTING w let i const - NIE ZADZIAŁA! będzie w "TYMACZASOWEJ STREFIE ŚMIERCI"
+		//  let i const
+		//	console.log(d); // wyrzuci błąd
+		//	let d = 22;
+		
 
 // HOISTING - przenoszenie DEKLARACJI zmiennej var i funkcji, na sam początek zakresu kodu.
 // Czyli, jeżeli jest w zakresie globalnym (funckja lub var), to na początek zakresu globalnego,
