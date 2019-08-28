402f039886fb13b2a0e6117a779c19df9337ade5
Karol Vogelgezang <karol.vogelgezang@domeny.pl>
2019-08-28 14:58:51 +0200

    ds

--- React-Notatki.js
+++ React-Notatki.js
@@ -1,11 +1,11 @@
-JSX
-	Why Using JSX in React?
-		React embraces the fact that rendering logic is inherently coupled with other UI logic: how events are handled, how the state changes over time, and how the data is prepared for display.
-		React doesn’t require using JSX, but most people find it helpful as a visual aid when working with UI inside the JavaScript code. It also allows React to show more useful error and warning messages.
+// JSX
+// 	Why Using JSX in React?
+// 		React embraces the fact that rendering logic is inherently coupled with other UI logic: how events are handled, how the state changes over time, and how the data is prepared for display.
+// 		React doesn’t require using JSX, but most people find it helpful as a visual aid when working with UI inside the JavaScript code. It also allows React to show more useful error and warning messages.
 
-	Embedding Expressions in JSX
-		You can put any valid JavaScript EXPRESSION (EXPRESSION -> An expression is any valid unit of code that resolves to a value) inside the curly braces in JSX. For example, 2 + 2, user.firstName, or formatName(user) are all valid JavaScript expressions.
-			Przykład:
+// 	Embedding Expressions in JSX
+// 		You can put any valid JavaScript EXPRESSION (EXPRESSION -> An expression is any valid unit of code that resolves to a value) inside the curly braces in JSX. For example, 2 + 2, user.firstName, or formatName(user) are all valid JavaScript expressions.
+// 			Przykład:
 				function formatName(user) {
 				  return user.firstName + ' ' + user.lastName;
 				}
@@ -26,9 +26,9 @@ JSX
 				  document.getElementById('root')
 				);
 
-	JSX is an Expression Too
-		This means that you can use JSX inside of if statements and for loops, assign it to variables, accept it as arguments, and return it from functions:
-			Przykład:
+	// JSX is an Expression Too
+	// 	This means that you can use JSX inside of if statements and for loops, assign it to variables, accept it as arguments, and return it from functions:
+	// 		Przykład:
 				function getGreeting(user) {
 				  if (user) {
 				    return <h1>Hello, {formatName(user)}!</h1>;
@@ -36,33 +36,33 @@ JSX
 				  return <h1>Hello, Stranger.</h1>;
 				}
 
-	JSX Prevents Injection Attacks -> To jako dodatkowa informacja, którą warto wiedzieć jako ciekawostka (Na romozwach moze tego nie mów, bo mogą zapytać jak blokuje przed wstrzyknięciem)
+	// JSX Prevents Injection Attacks -> To jako dodatkowa informacja, którą warto wiedzieć jako ciekawostka (Na romozwach moze tego nie mów, bo mogą zapytać jak blokuje przed wstrzyknięciem)
 
-	Warning:
-		Since JSX is closer to JavaScript than to HTML, React DOM uses camelCase property naming convention instead of HTML attribute names.
-		For example, class becomes className in JSX, and tabindex becomes tabIndex.
+	// Warning:
+	// 	Since JSX is closer to JavaScript than to HTML, React DOM uses camelCase property naming convention instead of HTML attribute names.
+	// 	For example, class becomes className in JSX, and tabindex becomes tabIndex.
 
 
 
-Rendering Elements
-	Elements are the smallest building blocks of React apps.
+// Rendering Elements
+// 	Elements are the smallest building blocks of React apps.
 		 const element = <h1>Hello, world</h1>;
 
-	Unlike browser DOM elements, React elements are plain objects, and are cheap to create. React DOM takes care of updating thw DOM to match the React elements.
+	// Unlike browser DOM elements, React elements are plain objects, and are cheap to create. React DOM takes care of updating thw DOM to match the React elements.
 
-	Note:
-		One might confuse elements with a more widely known consept of "components". We will introduce components in the next section. Elements are what components are "mede of", and we encourage you to read this section before jumping ahead.
+	// Note:
+	// 	One might confuse elements with a more widely known consept of "components". We will introduce components in the next section. Elements are what components are "mede of", and we encourage you to read this section before jumping ahead.
 
-	Rendering an Element into the DOM
+	// Rendering an Element into the DOM
 		const element = <h1>Hello, world</h1>;
 		ReactDOM.render(element, document.getElementById('root'));
 
-	Updating the Rendered ELement
-		React elements are immutable. Once you create an element, you can't change its children oe attributes. An element is like a single frame ina a movie: it represents the UI at a certain point in time.
+	// Updating the Rendered ELement
+	// 	React elements are immutable. Once you create an element, you can't change its children oe attributes. An element is like a single frame ina a movie: it represents the UI at a certain point in time.
 
-		With our knowledge so far (Później nauczymy się to robić inaczej, bo w praktyce inaczej się to robi, ale na ten moment chcemy wiedzieć to co tutaj), the only way to update the UI is to create a new element, and pass it to ReactDOM.render().
+	// 	With our knowledge so far (Później nauczymy się to robić inaczej, bo w praktyce inaczej się to robi, ale na ten moment chcemy wiedzieć to co tutaj), the only way to update the UI is to create a new element, and pass it to ReactDOM.render().
 
-		Consider this ticking clock example:
+	// 	Consider this ticking clock example:
 
 			function tick() {
 				const element = (
@@ -76,52 +76,52 @@ Rendering Elements
 
 			setIntreval(tick, 1000);
 
-		It calls ReactDOM.render() every second from a setInterval() callback.
+		// It calls ReactDOM.render() every second from a setInterval() callback.
 
-		Note:
-		In practice, most React apps only call ReactDOM.render() once. In the next sections we will learn how such code gets encapsulated into stateful components.
+		// Note:
+		// In practice, most React apps only call ReactDOM.render() once. In the next sections we will learn how such code gets encapsulated into stateful components.
 
-		That's we You should don't skip topics because they build on each other.
+		// That's we You should don't skip topics because they build on each other.
 
-		React only update's what's necessary
+		// React only update's what's necessary
 
-			You can check it with example with time 
-			"setIntreval(tick, 1000);"
+	// 		You can check it with example with time
+	// 		"setIntreval(tick, 1000);"
 
-		Even though we create an element describing the whole UI tree on every tick, only the text node whose contents has changed gets updated by React DOM.
+	// 	Even though we create an element describing the whole UI tree on every tick, only the text node whose contents has changed gets updated by React DOM.
 
-		In our experience, thinking about how the UI should look at any given moment rather than how to change t over time eliminates a whole class of bugs.
+	// 	In our experience, thinking about how the UI should look at any given moment rather than how to change t over time eliminates a whole class of bugs.
 
-	Components and Props
-		Components let You split rhe UI into independent, reausable pieces, and think about each piece in isolation. 
+	// Components and Props
+	// 	Components let You split rhe UI into independent, reausable pieces, and think about each piece in isolation.
 
-		Function and Class Components
-			The simplest way to define a component is to write a JavaScript function:
+	// 	Function and Class Components
+	// 		The simplest way to define a component is to write a JavaScript function:
 
 			function Welcome(props) {
 				return <h1>Hello, {props.name}</h1>;
 			}
 
-			This function is a valid React component because it accepts a single "props" (which stands for properties) object argument with data and returns a React element. We call such components "function components"  beacuse they are literally JavaScript functions.
-
-			You can also use an ES6 to define a component:
+			// This function is a valid React component because it accepts a single "props" (which stands for properties) object argument with data and returns a React element. We call such components "function components"  beacuse they are literally JavaScript functions.
+			//
+			// You can also use an ES6 to define a component:
 			class Welcome extends React.Component {
 				render() {
 					return <h1>Hello, {thos.props.name}</h1>;
 				}
 			}
 						
-			The above two components are equivalent from React's point of view.
+		// 	The above two components are equivalent from React's point of view.
 
-			Classes have some additional features that we will discuss in the next sections. Untill then, we will use function components for their conciseness
+		// 	Classes have some additional features that we will discuss in the next sections. Untill then, we will use function components for their conciseness
 
-		Rendering a Component
-			Previosly, we only encountered React elements that represent DOM tags:
+		// Rendering a Component
+		// 	Previosly, we only encountered React elements that represent DOM tags:
 				const element = <div />;
-			However, elements can also represent user-defined components"
+			// However, elements can also represent user-defined components"
 				const element = <Welcome name="Sara" />;
 
-			Example
+			// Example
 				function Welcome(props) {
 					return <h1>Hello, {props.name}</h1>;
 				}
@@ -129,19 +129,43 @@ Rendering Elements
 				const element = <Welcome name="Sara" />;
 				ReactDOM.render(element, document.getElementById('root'));
 
-			Let's recap what happens in this example:
-				1. We call ReactDOM.render() with the <Welocme name="Sara" /> element
-				2. React calls the Welocme component with {name: 'Sara'} as the props.
-				3. Our Welcome component returns a <h1>Hello, Sara</h1> element as the result.
-				4. React DOM efficiently updates the DOM to match <h1>Hello, Sara</h1>.
+		// 	Let's recap what happens in this example:
+		// 		1. We call ReactDOM.render() with the <Welocme name="Sara" /> element
+		// 		2. React calls the Welocme component with {name: 'Sara'} as the props.
+		// 		3. Our Welcome component returns a <h1>Hello, Sara</h1> element as the result.
+		// 		4. React DOM efficiently updates the DOM to match <h1>Hello, Sara</h1>.
 
-			Note:
-				Always start component names with a capital letter. React treats components starting with a lowercase letters as DOM tags. For example, <div/> represents an HTML div tag, but <Welcome /> represents a component and requires Welcome to be in scope.
+		// 	Note:
+		// 		Always start component names with a capital letter. React treats components starting with a lowercase letters as DOM tags. For example, <div/> represents an HTML div tag, but <Welcome /> represents a component and requires Welcome to be in scope.
 
-		Composing Components
-			Typically, new React apps have a single App component at the very top. However, if you integrate React into an existing app, you might start bottom-up with a small component like Button and gradually work your way to the top ot the view hierarchy.
+		// Composing Components
+		// 	Typically, new React apps have a single App component at the very top. However, if you integrate React into an existing app, you might start bottom-up with a small component like Button and gradually work your way to the top ot the view hierarchy.
 
-		Extracting Components ( TU SKOŃCZYŁEŚ )( TU SKOŃCZYŁEŚ )( TU SKOŃCZYŁEŚ )( TU SKOŃCZYŁEŚ )( TU SKOŃCZYŁEŚ )( TU SKOŃCZYŁEŚ )( TU SKOŃCZYŁEŚ )
+		// Extracting Components
+			// Nie bój się dzielić komponenty na mniejsze.
+			// Przykład:
+				function Comment(props) {
+					return (
+						<div className="Comment">
+							<div className="UserInfo">
+								<img className="Avatar"
+										 src={props.author.avatarUrl}
+										 alt={props.author.name}
+								/>
+								<div className="UserInfo-name">
+									{props.author.name}
+								</div>
+							</div>
+							<div className="Comment-text">
+								{props.text}
+							</div>
+							<div className="Comment-date">
+								{formatDate(props.date)}
+							</div>
+						</div>
+					);
+				}
+
 
 
 React Hooks 
